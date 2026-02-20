2af2c660d24ce861ed1e6e3432429e4f31f3886b
DevROHIT11 <rohitlbhalekar1812@gmail.com>
2026-02-21 00:05:09 +0530

    Tailwind css

---
+++ 06-Tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+const App = ()=>{
+  return (
+    <>
+    <div className="bg-gray-600 , w-full , h-screen">
+      <nav className = "m-0 , p-1 , h-auto , bg-black , text-taupe-100" >I'm header</nav>
+    </div>
+    </>
+  )
+}
+
+export default App
