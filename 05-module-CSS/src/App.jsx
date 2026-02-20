503a2a3f8f1fa658df50ff583c494d67094c3a38
DevROHIT11 <rohitlbhalekar1812@gmail.com>
2026-02-20 23:30:44 +0530

    Css types

---
+++ 05-module-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css' ;
+import Button from "./components/Button/Button.jsx";
+import Header from "./components/Header/Header.jsx";
+
+const App = () =>{
+  return (
+    <>
+    <Header/>
+    <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ 05-module-CSS/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import style from './Button.module.css' ;
+
+const Button = () =>{
+    return (
+        <>
+        <button className ={style.btn}>I'm a button</button>
+        </>
+    )
+}
+
+export default Button
